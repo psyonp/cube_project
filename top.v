f78ce1b02cfab992b45bab49a1f8919cddba2dc9
Punya Syon Pandey <101074452+psyonp@users.noreply.github.com>
2025-11-11 18:56:51 -0500

    Update top.v

--- top.v
+++ top.v
@@ -1,7 +1,4 @@
-/*
- * Top module that connects logic, cube_drawer, and VGA together
- */
-module top(
+module cube(
     input CLOCK_50,
     input [3:0] SW,
     input [1:0] KEY,
@@ -17,23 +14,23 @@ module top(
 
     // Reset signal
     wire resetn = KEY[0];
-    
+
     // Wires for the 6 cube faces
-    wire [2:0] f1 [0:8];  // front
-    wire [2:0] f2 [0:8];  // back
-    wire [2:0] f3 [0:8];  // left
-    wire [2:0] f4 [0:8];  // right
-    wire [2:0] f5 [0:8];  // top
-    wire [2:0] f6 [0:8];  // bottom
-    
+    wire [2:0] f1 [0:8];   // front
+    wire [2:0] f2 [0:8];   // back
+    wire [2:0] f3 [0:8];   // left
+    wire [2:0] f4 [0:8];   // right
+    wire [2:0] f5 [0:8];   // top
+    wire [2:0] f6 [0:8];   // bottom
+
     // Wires connecting drawer to VGA
     wire [7:0] x;
     wire [6:0] y;
     wire [2:0] colour;
     wire plot;
-    
+
     // 1. Logic module - generates cube state based on user moves
-    logic logic_unit(
+    logic_f logic_unit(
         .SW(SW),
         .KEY(KEY),
         .f1(f1),
@@ -43,7 +40,7 @@ module top(
         .f5(f5),
         .f6(f6)
     );
-    
+
     // 2. Cube drawer - translates cube state into pixel signals
     cube_drawer drawer(
         .clk(CLOCK_50),
@@ -59,26 +56,24 @@ module top(
         .colour(colour),
         .plot(plot)
     );
-    
-    // 3. VGA adapter - sends pixel signals to screen
-    vga_adapter #(
-    .RESOLUTION("160x120"),
-    .COLOR_DEPTH(3)
-    ) VGA(
-    .resetn(resetn),
-    .clock(CLOCK_50),
-    .color(colour),        // Changed from "colour" to "color"
-    .x(x),
-    .y(y),
-    .write(plot),          // Changed from "plot" to "write"
-    .VGA_R(VGA_R),
-    .VGA_G(VGA_G),
-    .VGA_B(VGA_B),
-    .VGA_HS(VGA_HS),
-    .VGA_VS(VGA_VS),
-    .VGA_BLANK_N(VGA_BLANK_N),
-    .VGA_SYNC_N(VGA_SYNC_N),
-    .VGA_CLK(VGA_CLK)
-    );
 
+    // 3. VGA adapter - sends pixel data to VGA display
+    vga_adapter VGA(
+        .resetn(resetn),
+        .clock(CLOCK_50),
+        .color(colour),
+        .x(x),
+        .y(y),
+        .write(plot),
+        .VGA_R(VGA_R),
+        .VGA_G(VGA_G),
+        .VGA_B(VGA_B),
+        .VGA_HS(VGA_HS),
+        .VGA_VS(VGA_VS),
+        .VGA_BLANK_N(VGA_BLANK_N),
+        .VGA_SYNC_N(VGA_SYNC_N),
+        .VGA_CLK(VGA_CLK)
+    );
+    defparam VGA.RESOLUTION = "160x120";
+    
 endmodule
