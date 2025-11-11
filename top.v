cd6b2e6b1136db0c3af25b0a78c501da1abdde6d
Punya Syon Pandey <101074452+psyonp@users.noreply.github.com>
2025-11-11 17:12:00 -0500

    Update top.v

--- top.v
+++ top.v
@@ -1,36 +1,66 @@
 /*
- * main file that wraps logic
- *  and vga code together
+ * Top module that connects logic, cube_drawer, and VGA together
  */
+module top(
+    input CLOCK_50,
+    input [3:0] SW,
+    input [1:0] KEY,
+    output [9:0] VGA_R,
+    output [9:0] VGA_G,
+    output [9:0] VGA_B,
+    output VGA_HS,
+    output VGA_VS,
+    output VGA_BLANK_N,
+    output VGA_SYNC_N,
+    output VGA_CLK
+);
 
-
-module top(...);
-    // wires that connect modules together
-    wire [2:0] cube_colors[0:53];
+    // Reset signal
+    wire resetn = KEY[0];
+    
+    // Wires for the 6 cube faces
+    wire [2:0] f1 [0:8];  // front
+    wire [2:0] f2 [0:8];  // back
+    wire [2:0] f3 [0:8];  // left
+    wire [2:0] f4 [0:8];  // right
+    wire [2:0] f5 [0:8];  // top
+    wire [2:0] f6 [0:8];  // bottom
+    
+    // Wires connecting drawer to VGA
     wire [7:0] x;
     wire [6:0] y;
     wire [2:0] colour;
     wire plot;
-
-    // 1. generates cube colors
+    
+    // 1. Logic module - generates cube state based on user moves
     logic logic_unit(
-        .clk(CLOCK_50),
-        .resetn(resetn),
-        .colors(cube_colors)
+        .SW(SW),
+        .KEY(KEY),
+        .f1(f1),
+        .f2(f2),
+        .f3(f3),
+        .f4(f4),
+        .f5(f5),
+        .f6(f6)
     );
-
-    // 2. translate cube colors into pixel signals
+    
+    // 2. Cube drawer - translates cube state into pixel signals
     cube_drawer drawer(
         .clk(CLOCK_50),
         .resetn(resetn),
-        .colors(cube_colors),
+        .f1(f1),
+        .f2(f2),
+        .f3(f3),
+        .f4(f4),
+        .f5(f5),
+        .f6(f6),
         .x(x),
         .y(y),
         .colour(colour),
         .plot(plot)
     );
-
-    // 3. VGA library sends pixel signals to screen
+    
+    // 3. VGA adapter - sends pixel signals to screen
     vga_adapter VGA(
         .resetn(resetn),
         .clock(CLOCK_50),
@@ -47,4 +77,5 @@ module top(...);
         .VGA_SYNC_N(VGA_SYNC_N),
         .VGA_CLK(VGA_CLK)
     );
+
 endmodule
