fd9447999fcbd69806030f92e57da52ea4d200e6
Punya Syon Pandey <101074452+psyonp@users.noreply.github.com>
2025-11-11 21:14:30 -0500

    Update logic_f.v

--- logic_f.v
+++ logic_f.v
@@ -13,7 +13,38 @@ SW [3:1] should be choosing the state, which KEY[1] should execute.
 111 should do nothing
 
 */
+/*
+Switch 0 should be reversing the operation
+KEY[0] is reset to start.
+SW [3:1] should be choosing the state, which KEY[1] should execute.
+
+000 should be move front
+001 should be move back
+010 should be move left
+011 should be move right
+100 should be move top
+101 should be move bottom
+110 should do nothing
+111 should do nothing
+
+*/
+/*
+Switch 0 should be reversing the operation
+KEY[0] is reset to start.
+SW [3:1] should be choosing the state, which KEY[1] should execute.
+
+000 should be move front
+001 should be move back
+010 should be move left
+011 should be move right
+100 should be move top
+101 should be move bottom
+110 should do nothing
+111 should do nothing
+*/
+
 module logic_f(
+    input CLOCK_50,
     input [3:0] SW,
     input [1:0] KEY,
     output reg [2:0] f1 [0:8],  // front
@@ -21,7 +52,8 @@ module logic_f(
     output reg [2:0] f3 [0:8],  // left
     output reg [2:0] f4 [0:8],  // right
     output reg [2:0] f5 [0:8],  // top
-    output reg [2:0] f6 [0:8]   // bottom
+    output reg [2:0] f6 [0:8],  // bottom
+    output reg redraw
 );
 
     // Internal state storage
@@ -32,8 +64,39 @@ module logic_f(
     reg [2:0] top   [0:8];
     reg [2:0] bottom[0:8];
 
+    wire resetn = KEY[0];
     wire reverse = SW[0];
     wire [2:0] move_sel = SW[3:1];
+
+    // Enhanced button debouncing
+    reg [19:0] debounce_counter;  // ~10ms at 50MHz
+    reg key1_stable;
+    reg key1_prev;
+    wire key1_pressed;
+    
+    // Only trigger on stable low after debounce
+    assign key1_pressed = !key1_stable && key1_prev;
+    
+    // Debounce logic
+    always @(posedge CLOCK_50 or negedge resetn) begin
+        if (!resetn) begin
+            debounce_counter <= 0;
+            key1_stable <= 1'b1;
+            key1_prev <= 1'b1;
+        end else begin
+            key1_prev <= key1_stable;
+            
+            if (KEY[1] == key1_stable) begin
+                debounce_counter <= 0;
+            end else begin
+                debounce_counter <= debounce_counter + 1;
+                if (debounce_counter >= 500000) begin  // ~10ms
+                    key1_stable <= KEY[1];
+                    debounce_counter <= 0;
+                end
+            end
+        end
+    end
 
     wire [2:0] front_out_front [0:8], front_out_back [0:8], front_out_left [0:8];
     wire [2:0] front_out_right [0:8], front_out_top [0:8], front_out_bottom [0:8];
@@ -98,9 +161,9 @@ module logic_f(
 
     integer i;
 
-    // Combined sequential logic for both reset and move operations
-    always @(negedge KEY[0] or negedge KEY[1]) begin
-        if (!KEY[0]) begin
+    // Clock-based sequential logic with edge detection
+    always @(posedge CLOCK_50 or negedge resetn) begin
+        if (!resetn) begin
             // Reset to solved cube
             for (i = 0; i < 9; i = i + 1) begin
                 front[i]  <= 3'b000; // white
@@ -108,80 +171,93 @@ module logic_f(
                 left[i]   <= 3'b010; // blue
                 right[i]  <= 3'b011; // green
                 top[i]    <= 3'b100; // red
-                bottom[i] <= 3'b101; // orange
+                bottom[i] <= 3'b101; // orange/magenta
             end
-        end
-        else if (!KEY[1]) begin
-            // Execute selected rotation
-            case (move_sel)
-                3'b000: begin // front
-                    for (i = 0; i < 9; i = i + 1) begin
-                        front[i]  <= front_out_front[i];
-                        back[i]   <= front_out_back[i];
-                        left[i]   <= front_out_left[i];
-                        right[i]  <= front_out_right[i];
-                        top[i]    <= front_out_top[i];
-                        bottom[i] <= front_out_bottom[i];
+            redraw <= 1'b1;  // Trigger initial draw
+        end else begin
+            // Generate single-cycle redraw pulse after state update
+            if (redraw) begin
+                redraw <= 1'b0;
+            end
+            
+            // Execute rotation on button press
+            if (key1_pressed) begin
+                case (move_sel)
+                    3'b000: begin // front
+                        for (i = 0; i < 9; i = i + 1) begin
+                            front[i]  <= front_out_front[i];
+                            back[i]   <= front_out_back[i];
+                            left[i]   <= front_out_left[i];
+                            right[i]  <= front_out_right[i];
+                            top[i]    <= front_out_top[i];
+                            bottom[i] <= front_out_bottom[i];
+                        end
+                        redraw <= 1'b1;
                     end
-                end
-        
-                3'b001: begin // back
-                    for (i = 0; i < 9; i = i + 1) begin
-                        front[i]  <= back_out_front[i];
-                        back[i]   <= back_out_back[i];
-                        left[i]   <= back_out_left[i];
-                        right[i]  <= back_out_right[i];
-                        top[i]    <= back_out_top[i];
-                        bottom[i] <= back_out_bottom[i];
+            
+                    3'b001: begin // back
+                        for (i = 0; i < 9; i = i + 1) begin
+                            front[i]  <= back_out_front[i];
+                            back[i]   <= back_out_back[i];
+                            left[i]   <= back_out_left[i];
+                            right[i]  <= back_out_right[i];
+                            top[i]    <= back_out_top[i];
+                            bottom[i] <= back_out_bottom[i];
+                        end
+                        redraw <= 1'b1;
                     end
-                end
-        
-                3'b010: begin // left
-                    for (i = 0; i < 9; i = i + 1) begin
-                        front[i]  <= left_out_front[i];
-                        back[i]   <= left_out_back[i];
-                        left[i]   <= left_out_left[i];
-                        right[i]  <= left_out_right[i];
-                        top[i]    <= left_out_top[i];
-                        bottom[i] <= left_out_bottom[i];
+            
+                    3'b010: begin // left
+                        for (i = 0; i < 9; i = i + 1) begin
+                            front[i]  <= left_out_front[i];
+                            back[i]   <= left_out_back[i];
+                            left[i]   <= left_out_left[i];
+                            right[i]  <= left_out_right[i];
+                            top[i]    <= left_out_top[i];
+                            bottom[i] <= left_out_bottom[i];
+                        end
+                        redraw <= 1'b1;
                     end
-                end
-        
-                3'b011: begin // right
-                    for (i = 0; i < 9; i = i + 1) begin
-                        front[i]  <= right_out_front[i];
-                        back[i]   <= right_out_back[i];
-                        left[i]   <= right_out_left[i];
-                        right[i]  <= right_out_right[i];
-                        top[i]    <= right_out_top[i];
-                        bottom[i] <= right_out_bottom[i];
+            
+                    3'b011: begin // right
+                        for (i = 0; i < 9; i = i + 1) begin
+                            front[i]  <= right_out_front[i];
+                            back[i]   <= right_out_back[i];
+                            left[i]   <= right_out_left[i];
+                            right[i]  <= right_out_right[i];
+                            top[i]    <= right_out_top[i];
+                            bottom[i] <= right_out_bottom[i];
+                        end
+                        redraw <= 1'b1;
                     end
-                end
-        
-                3'b100: begin // top
-                    for (i = 0; i < 9; i = i + 1) begin
-                        front[i]  <= top_out_front[i];
-                        back[i]   <= top_out_back[i];
-                        left[i]   <= top_out_left[i];
-                        right[i]  <= top_out_right[i];
-                        top[i]    <= top_out_top[i];
-                        bottom[i] <= top_out_bottom[i];
+            
+                    3'b100: begin // top
+                        for (i = 0; i < 9; i = i + 1) begin
+                            front[i]  <= top_out_front[i];
+                            back[i]   <= top_out_back[i];
+                            left[i]   <= top_out_left[i];
+                            right[i]  <= top_out_right[i];
+                            top[i]    <= top_out_top[i];
+                            bottom[i] <= top_out_bottom[i];
+                        end
+                        redraw <= 1'b1;
                     end
-                end
-        
-                3'b101: begin // bottom
-                    for (i = 0; i < 9; i = i + 1) begin
-                        front[i]  <= bottom_out_front[i];
-                        back[i]   <= bottom_out_back[i];
-                        left[i]   <= bottom_out_left[i];
-                        right[i]  <= bottom_out_right[i];
-                        top[i]    <= bottom_out_top[i];
-                        bottom[i] <= bottom_out_bottom[i];
+            
+                    3'b101: begin // bottom
+                        for (i = 0; i < 9; i = i + 1) begin
+                            front[i]  <= bottom_out_front[i];
+                            back[i]   <= bottom_out_back[i];
+                            left[i]   <= bottom_out_left[i];
+                            right[i]  <= bottom_out_right[i];
+                            top[i]    <= bottom_out_top[i];
+                            bottom[i] <= bottom_out_bottom[i];
+                        end
+                        redraw <= 1'b1;
                     end
-                end
-        
-                default: begin end // do nothing
-            endcase
+            
+                    default: begin end // do nothing
+                endcase
+            end
         end
     end
 
@@ -587,5 +663,3 @@ always @(*) begin
 end
 
 endmodule
-
-
